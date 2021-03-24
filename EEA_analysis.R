0260a3426a606a68b10ed450bf4d1366b514d6e3
klapierre <lapierre.kimberly@gmail.com>
2021-03-24 10:25:49 -0600

    analysis file

---
+++ EEA_analysis.R
@@ -0,0 +1,76 @@
+library(performance)
+library(tidyverse)
+
+#kim's laptop
+setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\misc co-author papers\\Fort Keogh_drought x mowing\\EEA data')
+
+
+###functions
+#function to get means and standard errors for easy bar graphs
+#barGraphStats(data=, variable="", byFactorNames=c(""))
+barGraphStats <- function(data, variable, byFactorNames) {
+  count <- length(byFactorNames)
+  N <- aggregate(data[[variable]], data[byFactorNames], FUN=length)
+  names(N)[1:count] <- byFactorNames
+  names(N) <- sub("^x$", "N", names(N))
+  mean <- aggregate(data[[variable]], data[byFactorNames], FUN=mean)
+  names(mean)[1:count] <- byFactorNames
+  names(mean) <- sub("^x$", "mean", names(mean))
+  sd <- aggregate(data[[variable]], data[byFactorNames], FUN=sd)
+  names(sd)[1:count] <- byFactorNames
+  names(sd) <- sub("^x$", "sd", names(sd))
+  preSummaryStats <- merge(N, mean, by=byFactorNames)
+  finalSummaryStats <- merge(preSummaryStats, sd, by=byFactorNames)
+  finalSummaryStats$se <- finalSummaryStats$sd / sqrt(finalSummaryStats$N)
+  return(finalSummaryStats)
+}  
+
+
+###set the theme of graphs
+theme_set(theme_bw())
+theme_update(axis.title.x=element_text(size=20, vjust=-0.35), axis.text.x=element_text(size=16),
+             axis.title.y=element_text(size=20, angle=90, vjust=0.5), axis.text.y=element_text(size=16),
+             plot.title = element_text(size=24, vjust=2),
+             panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
+             legend.title=element_text(size=20), legend.text=element_text(size=20))
+
+
+###source data
+source('C:\\Users\\lapie\\Desktop\\R files laptop\\FK_drought-mowing\\EEA_fluorescence.R')
+
+
+
+
+
+###running ANOVAs
+summary(apANOVA <- aov(activity_mean ~ precip*mow*seas, data=subset(fluorTrt, enzyme=='AP'))) #no significant effects
+check_model(apANOVA)
+
+summary(bgANOVA <- aov(activity_mean ~ precip*mow*seas, data=subset(fluorTrt, enzyme=='BG'))) #significant effect of season of mowing: mowing on October leads to higher activity than mowing in June
+check_model(bgANOVA)
+
+summary(bxANOVA <- aov(activity_mean ~ precip*mow*seas, data=subset(fluorTrt, enzyme=='BX'))) #no significant effects
+check_model(bxANOVA)
+
+summary(cbANOVA <- aov(activity_mean ~ precip*mow*seas, data=subset(fluorTrt, enzyme=='CB'))) #no significant effects
+check_model(cbANOVA)
+
+summary(lapANOVA <- aov(activity_mean ~ precip*mow*seas, data=subset(fluorTrt, enzyme=='LAP'))) #no significant effects
+check_model(lapANOVA)
+
+summary(nagANOVA <- aov(activity_mean ~ precip*mow*seas, data=subset(fluorTrt, enzyme=='NAG'))) #significant effect of season of mowing: mowing on October leads to higher activity than mowing in June
+check_model(nagANOVA)
+
+
+#figures
+ggplot(data=fluorTrt, aes(x=precip, y=activity_mean, color=interaction(mow,seas))) +
+  geom_boxplot() +
+  xlab('Precipitation (%)') + ylab('Extracellular Enzyme Activity (nmol/hr/g)') +
+  scale_color_brewer(palette='Paired', name='Mowing') +
+  facet_wrap(~enzyme, scales='free')
+
+ggplot(data=barGraphStats(data=fluorTrt, variable="activity_mean", byFactorNames=c("seas", "enzyme")), aes(x=seas, y=mean)) +
+  geom_bar(stat='identity') +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.2) +
+  xlab('Mowing Season') + ylab('Extracellular Enzyme Activity (nmol/hr/g)') +
+  facet_wrap(~enzyme, scales='free')
